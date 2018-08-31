ef265770cd5a32a776b1688a6e84f70db1d2441c
Mohammed Elamin <melamin100@yahoo.com>
2018-08-31 00:01:38 -0700

    first commit

---
+++ asd.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mohammed Elamin
+ * Date: 7/31/2018
+ * Time: 8:44 PM
+ */
+
+
+
+test::name();
---
+++ test.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mohammed Elamin
+ * Date: 7/31/2018
+ * Time: 8:44 PM
+ */
+
+class test
+{
+
+
+    public  static  function  name(){
+        echo  'hi there';
+    }
+
+}
